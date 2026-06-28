d9d1e863767dfd1e1c7786019808cf1c8f0861b1
timelessp <timelessp@users.noreply.github.com>
2026-06-28 13:36:26 +0100

    Version bump: 20260628-13

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ from typing import cast
 import app.storage as storage_module
 
 app = FastAPI(title="Quipu MVP", version="0.1.2", max_upload_size=20 * 1024 * 1024)  # Set limit to 20MB
-ASSET_VERSION = "20260628-12"
+ASSET_VERSION = "20260628-13"
 app.include_router(google_router)
 
 app.add_middleware(
